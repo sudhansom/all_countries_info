f6c57057a97629ccf24e6c1bc9a080743c45dbfd
bhuwan <bkspoudel@gmail.com>
2021-09-09 16:29:45 +0200

    not getting result from hook as aspected

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css'
+import useCountries from './custom-hooks/useCountries'
+import useCountry from './custom-hooks/useCountry'
 
 function App() {
-  return <div className='App'></div>
+  const url = "https://restcountries.eu/rest/v2/all";
+  const [error, countries] = useCountries(url)
+  const foundCountry = countries.find(elem => elem.name==="Nepal")
+  //console.log("country name:",countries[0].name)
+  
+  return <div className='App'>
+  
+  </div>
 }
 
 export default App
--- src/custom-hooks/useCountries.js
+++ src/custom-hooks/useCountries.js
@@ -1,1 +1,21 @@
-// This hook is used to fetch all countries
+import { useState, useEffect } from "react";
+const useCountries = (url)=>{
+    const [data, setData] = useState([])
+    const [error, setError] = useState('');
+    useEffect(()=>{
+        const fetchData = async()=>{
+            try{
+                const result = await fetch(url).then(response=>response.json());
+                console.log(result[0].name)// its fine here ...i want result like this in App.js
+                setData(result) // sets data = result
+                
+            }
+            catch(err){
+                setError(err)
+            }
+        }
+        fetchData()
+    }, [url])
+    return [error, data]
+}
+export default useCountries
