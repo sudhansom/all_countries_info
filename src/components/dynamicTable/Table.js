347883ef78011281766b6520a21c9899238e9cb1
bhuwan <bkspoudel@gmail.com>
2022-03-11 08:49:17 +0100

    tried to put key props to remove the warnings

--- src/components/dynamicTable/Table.js
+++ src/components/dynamicTable/Table.js
@@ -1,55 +1,42 @@
-import React from 'react'
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import React from "react";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
-
-function DataTable({items, columns}) {
-    if(items.length>0){
-        return (
-        <TableContainer>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        {
-                            columns.map(col=>{
-                                return(
-                                    <TableCell>{col.label}</TableCell>
-                                )
-                            })
-                        }
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                        {
-                            items.map(element=>{
-                                return (
-                                    <TableRow>
-                                        { 
-                                            columns.map(c=>{
-                                                return (
-                                                    <TableCell>
-                                                        {
-                                                            c.renderContent(element)
-                                                        }
-                                                    </TableCell>
-                                                )
-                                            })
-                                        }
-                                    </TableRow>
-                                )
-                            })
-                        }
-                </TableBody>
-            </Table>
-        </TableContainer>
-    )
-    }else{
-        return <p>Nothing to display...</p>
-    }
+function DataTable({ items, columns }) {
+  if (items.length > 0) {
+    return (
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {columns.map((col) => {
+                return <TableCell>{col.label}</TableCell>;
+              })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {items.map((element, index) => {
+              return (
+                <TableRow key={index}>
+                  {columns.map((c, i) => {
+                    return (
+                      <TableCell key={i}>{c.renderContent(element)}</TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  } else {
+    return <p>Nothing to display...</p>;
+  }
 }
 
-export default DataTable
+export default DataTable;
